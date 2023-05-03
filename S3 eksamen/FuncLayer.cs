761551bef780e4452290aabd5840774edfe64f02
marku <marku@DESKTOP-T9HQH0R>
2023-05-03 13:03:43 +0200

    Add project files.

---
+++ S3 eksamen/FuncLayer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S3_eksamen
+{
+    class FuncLayer : INotifyPropertyChanged
+    {
+        public Model Data { get; set; } = new Model();
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void RaisePropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new
+                PropertyChangedEventArgs(propName));
+            }
+        }
+
+
+        public void OpretKunde(string kundeNavn, int KundeAdresse, int kundeTelefon)
+        {
+            Kunde kunde;
+            vare = new Vare();
+            vare.Navn = navn;
+            vare.Pris = pris;
+            vare.Indkøbspris = indkøbspris;
+            vare.Beskrivelse = beskrivelse;
+            Data.OpretVare(vare);
+            RaisePropertyChanged(nameof(Vareoversigt));
+        }
+    }
+}
---
+++ S3 eksamen/Model.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S3_eksamen
+{
+    class Model : DbContext
+    {
+        public DbSet<Bog> Bøger { get; set; }
+        public DbSet<Rejsearrangement> Rejsearrangementer { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EFViking; Trusted_Connection = True; ");
+        }
+
+        public class Bog
+        {
+            public int Id { get; set; }
+            public string Forfatter { get; set; }
+            public string Titel { get; set; }
+            public string Udgiver { get; set; }
+            public int Udgivelsesår { get; set; }
+            public string Eksemplarer { get; set; }
+            public int ISBN { get; set; }
+
+        }
+
+        public class Lån
+        {
+            public int Id { get; set; }
+            public int Dato { get; set; }
+            public string Låner { get; set; }
+            public string LånteBog { get; set; }
+            public int Antal { get; set; }
+        }
+    }
+}
